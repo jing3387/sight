d003f9e31435646fba40fbfab919b5d5f7b68a54
Jarrod Jeffrey Ingram <jing0012@uni.sydney.edu.au>
2014-12-30 22:36:18 +1100

    OS X fix

--- src/Main.java
+++ src/Main.java
@@ -338,6 +338,10 @@ public class Main {
         if (glfwInit() != GL_TRUE)
             throw new IllegalStateException("Unable to initialize GLFW");
         glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
         glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
         int WIDTH = 800;
